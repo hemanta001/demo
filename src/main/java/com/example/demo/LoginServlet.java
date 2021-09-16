a2ace7ce048643217a28039c7619687d48fc6d3b
Hemanta Ghimire <rockingguyheman.hg@gmail.com>
2021-09-16 21:20:02 +0545

    first commit

---
+++ src/main/java/com/example/demo/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        RequestDispatcher dispatcher = request.getRequestDispatcher(
+//                "/WEB-INF/home/home.jsp");
+//        dispatcher.forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String email=request.getParameter("email");
+        String password=request.getParameter("password");
+        System.out.println(email);
+        System.out.println(password);
+
+        if(email.equals("rockingguyheman.hg@gmail.com") && password.equals("hem123...")){
+            RequestDispatcher dispatcher = request.getRequestDispatcher(
+                    "/WEB-INF/home/home.jsp");
+            dispatcher.forward(request, response);
+        }
+        else{
+            response.sendRedirect("/demo_war_exploded");
+        }
+
+    }
+
+    public void destroy() {
+    }
+
+}
---
+++ target/demo-1.0-SNAPSHOT/WEB-INF/home/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: deerholdnepal
+  Date: 16/09/2021
+  Time: 20:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>I am at home page</h1>
+</body>
+</html>
